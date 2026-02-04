b3991d232c9bd4cea8d834928d56664913808f17
Aryan <aryankawale163@gmail.com>
2026-02-04 22:46:34 +0530

    feat: add Python scripts for explicit type conversion and exploring tuple functionalities.

---
+++ tupleListArray/tupple.py
@@ -0,0 +1,62 @@
+# t1 = (101, "aryan", 22)
+# t2 = ("aryan","Devidas","Kawale")
+# t3=20,50,20,10
+# print(type(t1))
+# print(type(t2))
+# print(type(t2))
+
+
+# tup = ("hello",)
+# print(type(tup))
+# tup2 = "hello"
+# print(type(tup2))
+
+
+# tuple1=(10,20,30,40,50,60,70,80,90)
+# print(tuple1)
+# count = 0
+# for i in tuple1:
+#     print("tuple1[%d]=[%d]"%(count,i))
+#     count=count+1
+
+# tuple2 = tuple(input("Enter the numer of Elements:"))
+# print(tuple2)
+# count = 0
+# for i in tuple2:
+#     print("tuple2[%d]=[%s]" % (count, i))
+#     count = count + 1
+
+
+# tup = (1,2,3,4,5,6,7)
+# print(tup[0])
+# print(tup[1])
+# print(tup[2])
+# # It will give the IndexError
+# print(tup[8])
+
+
+# tuple = (1,2,3,4,5,6,7)
+# #element 1 to end
+# print(tuple[1:])
+# #element 0 to 3 element
+# print(tuple[:4])
+# #element 1 to 4 element
+# print(tuple[1:5])
+# # element 0 to 6 and take step of 2 print(tuple[0:6:2])
+
+
+# tuple1 = (1, 2, 3, 4, 5)
+# print(tuple1[-1])#5
+# print(tuple1[-4])#2
+# print(tuple1[-3:-1])#3,4
+# print(tuple1[:-1])#1,2,3,4
+# print(tuple1[-2:])#4,5
+
+
+# can't delete elements in tuple due to tuples are immutales
+# t1 = (1, 2, 3, 4, 5, 6)
+# print(t1)
+# del t1[0]
+# print(t1)
+# del t1
+# print(t1)
