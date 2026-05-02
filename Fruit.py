76915a050dc945c4a328d648c0b419a1a77dbcb3
Aryan <aryankawale163@gmail.com>
2026-05-02 17:06:50 +0530

    feat: add Fruit.py to identify banana ripeness based on color

---
+++ Fruit.py
@@ -0,0 +1,10 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Yellow":
+        print("Ripe")
+    elif color == "Green":
+        print("Unripe")
+    elif color == "Brown":
+        print("OverRipe")
