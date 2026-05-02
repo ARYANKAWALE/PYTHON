f488d70f00689ff4fab9f87caadaa57a0f7178bf
Aryan <aryankawale163@gmail.com>
2026-05-02 16:17:42 +0530

    feat: add scripts for age categorization and random list selection

---
+++ ageCategory.py
@@ -0,0 +1,9 @@
+age = int(input("Enter Your age:"))
+if age < 13:
+    print("this is the Child Age")
+if 13 <= age <=19:
+    print("Teenager")
+if 20 <= age <=59:
+    print("Adult")
+if age > 60:
+    print("Senior")
---
+++ random_choice.py
@@ -0,0 +1,3 @@
+import random
+l1 = ['Lamborghini','Ferrari','Mitsubishi','Lexus']
+random.choice(l1)
