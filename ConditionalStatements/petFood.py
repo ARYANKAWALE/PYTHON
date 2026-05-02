275c49952a0a83cc82c0373ac1be47c5096e0f47
Aryan <aryankawale163@gmail.com>
2026-05-02 18:15:14 +0530

    feat: add script to determine pet food type based on age using conditional statements

---
+++ ConditionalStatements/petFood.py
@@ -0,0 +1,15 @@
+dogAge = 5
+catAge = 7
+
+if dogAge < 2:
+    dog_food = "Puppy food"
+else:
+    dog_food = "Adult dog food"
+
+if catAge > 5:
+    cat_food = "Senior cat food"
+else:
+    cat_food = "Regular cat food"
+
+print(f"Dog age {dogAge}: {dog_food}")
+print(f"Cat age {catAge}: {cat_food}")
